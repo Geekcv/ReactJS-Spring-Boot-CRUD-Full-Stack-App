0deceae628058671fd78c1fef4a2255e946c2395
Abhishek <geekcv2022@gmail.com>
2024-06-16 09:58:37 +0530

    mongodb configration add
    all file chnage

--- springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,56 +1,117 @@
 package net.javaguides.springboot.model;
 
-import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
-@Table(name = "employees")
+// import jakarta.persistence.*;
+
+// @Entity
+// @Table(name = "employees")
+@Document(collection = "employees")
 public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	
-	@Column(name = "first_name")
-	private String firstName;
 
-	@Column(name = "last_name")
+	@Id
+	private String id; // Use String type for MongoDB ID
+	private String firstName;
 	private String lastName;
-	
-	@Column(name = "email_id")
 	private String emailId;
-	
+
 	public Employee() {
-		
 	}
-	
+
 	public Employee(String firstName, String lastName, String emailId) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.emailId = emailId;
 	}
-	public long getId() {
+
+	public String getId() {
 		return id;
 	}
-	public void setId(long id) {
+
+	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
+	// FOR My SQL OR RDMS
+	// @Id
+	// @GeneratedValue(strategy = GenerationType.IDENTITY)
+	// private long id;
+
+	// @Column(name = "first_name")
+	// private String firstName;
+
+	// @Column(name = "last_name")
+	// private String lastName;
+
+	// @Column(name = "email_id")
+	// private String emailId;
+
+	// public Employee() {
+
+	// }
+
+	// public Employee(String firstName, String lastName, String emailId) {
+	// super();
+	// this.firstName = firstName;
+	// this.lastName = lastName;
+	// this.emailId = emailId;
+	// }
+
+	// public long getId() {
+	// return id;
+	// }
+
+	// public void setId(long id) {
+	// this.id = id;
+	// }
+
+	// public String getFirstName() {
+	// return firstName;
+	// }
+
+	// public void setFirstName(String firstName) {
+	// this.firstName = firstName;
+	// }
+
+	// public String getLastName() {
+	// return lastName;
+	// }
+
+	// public void setLastName(String lastName) {
+	// this.lastName = lastName;
+	// }
+
+	// public String getEmailId() {
+	// return emailId;
+	// }
+
+	// public void setEmailId(String emailId) {
+	// this.emailId = emailId;
+	// }
 }
--- springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,11 +1,17 @@
 package net.javaguides.springboot.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import net.javaguides.springboot.model.Employee;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+// my sql
+// public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
+// }
+
+// mondoDb
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }
